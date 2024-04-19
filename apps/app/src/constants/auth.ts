2a28110d20fc33a68dc1c09e29bc73cf12fbf1fc
rharkor <louis@huort.com>
2024-04-19 21:55:44 +0200

    refactor: redirect url

--- apps/app/src/constants/auth.ts
+++ apps/app/src/constants/auth.ts
@@ -3,7 +3,7 @@ export const SESSION_MAX_AGE = 360 * 24 * 60 * 60 // 360 days
 export const authRoutes = {
   signIn: ["/sign-in", "/login", "/signin"],
   signUp: ["/sign-up", "/register", "/signup"],
-  redirectAfterSignIn: "/examples/profile",
+  redirectAfterSignIn: "/",
   redirectOnUnhauthorized: "/sign-in",
 }
 
