bc7090455979c540d26a7fd6b99d1951428f726e
rharkor <louis@huort.com>
2024-05-09 18:53:34 +0200

    refactor: do not redirect admin

--- apps/app/src/app/[lang]/(protected)/layout.tsx
+++ apps/app/src/app/[lang]/(protected)/layout.tsx
@@ -4,6 +4,7 @@ import { redirect } from "next/navigation"
 import requireAuth from "@/components/auth/require-auth"
 import BottomBar from "@/components/navigation/bottom-bar"
 import { BottomBarDr } from "@/components/navigation/bottom-bar.dr"
+import { rolesAsObject } from "@/constants"
 import SigningOutProvider from "@/contexts/signing-out/provider"
 import VerifyEmailProvider from "@/contexts/verify-email/provider"
 import { VerifyEmailDr } from "@/contexts/verify-email/verify-email.dr"
@@ -36,7 +37,7 @@ export default async function ProtectedLayout({
   const hasPetProfile = account.user.hasPetProfile
 
   const headersStore = headers()
-  if (!account.user.hasPetProfile) {
+  if (!account.user.hasPetProfile && account.user.role !== rolesAsObject.admin) {
     const pathname = headersStore.get("x-url")
     const petProfilePath = "/profile/pet-profile"
     if (!pathname?.includes(petProfilePath)) redirect(petProfilePath + "?onSuccess=/")
